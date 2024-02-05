828e455d6fe59cad635b99217d284863e1f38bac
little-junior <rampagedolol@outlook.com>
2024-02-05 15:31:52 -0300

    feat: add Subtarefa initial implementation

--- ProcessadorTarefas/Entidades/Subtarefa.cs
+++ ProcessadorTarefas/Entidades/Subtarefa.cs
@@ -3,6 +3,17 @@
     public struct Subtarefa
     {
         public TimeSpan Duracao { get; set; }
+
+        public Subtarefa()
+        {
+            var randomNumber = new Random().Next(3, 61);
+            Duracao = new TimeSpan(0, 0, randomNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"Subtarefa com duracao de {Duracao.Seconds} segundos";
+        }
     }
 
 }
