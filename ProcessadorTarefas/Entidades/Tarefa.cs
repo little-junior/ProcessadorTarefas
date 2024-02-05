e6c5b94541e318efd346d12cdf6e458f9b3acff2
little-junior <rampagedolol@outlook.com>
2024-02-05 15:31:39 -0300

    feat: add Tarefa initial implementation

--- ProcessadorTarefas/Entidades/Tarefa.cs
+++ ProcessadorTarefas/Entidades/Tarefa.cs
@@ -8,6 +8,25 @@
         public DateTime EncerradaEm { get; set; }
         public IEnumerable<Subtarefa> SubtarefasPendentes { get; set; }
         public IEnumerable<Subtarefa> SubtarefasExecutadas { get; set; }
-    }
 
+        public Tarefa(int maxSubTarefas)
+        {
+            Id = new Random().Next();
+            Estado = EstadoTarefa.Criada;
+            SubtarefasPendentes = GenerateSubTarefas(maxSubTarefas);
+            SubtarefasExecutadas = Array.Empty<Subtarefa>();
+        }
+
+        private IEnumerable<Subtarefa> GenerateSubTarefas(int maxSubTarefas)
+        {
+            var randomNumber = new Random().Next(10, maxSubTarefas + 1);
+
+            return Enumerable.Range(10, randomNumber).Select(index => new Subtarefa()).ToArray();
+        }
+
+        public override string ToString()
+        {
+            return $"Tarefa {Id} | Estado: {Estado} | Iniciada em: {IniciadaEm} | Encerrada Em: {EncerradaEm} |  Pendentes: {SubtarefasPendentes.Count()} | Executadas: {SubtarefasExecutadas?.Count()}";
+        }
+    }
 }
