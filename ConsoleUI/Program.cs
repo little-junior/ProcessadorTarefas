89320f012d161665e70476a0d33a95d3624177ce
little-junior <rampagedolol@outlook.com>
2024-02-13 22:35:50 -0300

    feat: add dependency injection of services

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,29 +12,47 @@ namespace ConsoleUI
     {
         static async Task Main(string[] args)
         {
-            Menu();
+            var serviceProvider = ConfigureServiceProvider();
+
+            var processador = serviceProvider.GetService<IProcessadorTarefas>();
+            var gerenciador = serviceProvider.GetService<IGerenciadorTarefas>();
+
+            await processador!.Iniciar();
+
+            while (true)
+            {
+                Menu();
+
+                Console.Clear();
+
+                await Task.Delay(100);
+            }
+
             
 
         }
 
-        //private static IServiceProvider ConfigureServiceProvider()
-        //{
+        private static IServiceProvider ConfigureServiceProvider()
+        {
 
-        //    IConfiguration configuration = new ConfigurationBuilder().SetBasePath();
+            IConfiguration configuration = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                            .Build();
 
 
-        //    IServiceCollection services = new ServiceCollection();
-        //    services.AddScoped(_ => configuration);
-        //    services.AddScoped<IRepository<Tarefa>, MemoryRepository>();
-        //    services.AddSingleton<IProcessadorTarefas, Processador>();
-        //    services.AddScoped<IGerenciadorTarefas, Gerenciador>(serviceProvider =>
-        //    {
-        //        var repository = serviceProvider.GetService<IRepository<Tarefa>>();
-        //        return new Gerenciador(repository, serviceProvider, configuration);
-        //    });
+            IServiceCollection services = new ServiceCollection();
+            services.AddScoped(_ => configuration);
+            services.AddScoped<IRepository<Tarefa>, MemoryRepository>();
+            services.AddSingleton<IProcessadorTarefas, Processador>();
+            services.AddScoped<IGerenciadorTarefas, Gerenciador>(serviceProvider =>
+            {
+                var repository = serviceProvider.GetService<IRepository<Tarefa>>();
+                return new Gerenciador(repository, serviceProvider, configuration);
+            });
 
-        //    return services.BuildServiceProvider();
-        //}
+            return services.BuildServiceProvider();
+        }
 
         static void Menu()
         {
